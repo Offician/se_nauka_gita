e107a90b82a14ca3f22f9b9763d5c1df76dbc10f
Offician <riccardopustizzi@gmail.com>
2026-05-17 17:57:13 +0200

    Dodano skrypt średniej ważonej

---
+++ weighted_avg.py
@@ -0,0 +1,24 @@
+if __name__ == "__main__":
+    list = []
+    weights = []
+    user_input = ""
+    while True:
+        user_input = input("Podaj liczbę całkowitą:")
+        if user_input == "exit" or user_input == "":
+            break
+        
+        weight = input("Podaj wagę liczby:")
+
+        list.append(int(user_input) * int(weight))
+        weights.append(int(user_input))
+        
+    sum = 0
+    weights_total = 0
+    for i in list:
+        sum += i
+
+    for w in weights:
+        weights_total += w
+
+    avg = sum / w
+    print(f"Twoja średnia ważona to: {avg}")
