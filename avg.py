0235f13f28848bd08db1618d74b61b6db2397d64
Offician <riccardopustizzi@gmail.com>
2026-05-17 17:47:51 +0200

    Dodano skrypt funkcji obliczającej średnią

---
+++ avg.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    list = []
+    user_input = ""
+    while True:
+        user_input = input("Podaj liczbę całkowitą:")
+        if user_input == "exit" or user_input == "":
+            break
+        user_input = int(user_input)
+        list.append(user_input)
+        
+    sum = 0
+    for i in list:
+        sum += i
+    avg = sum / list.__len__()
+    print(f"Twoja średnia to: {avg}")
